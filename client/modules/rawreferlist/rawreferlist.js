6d9464b8a32e1fbc5e936446ddfd336fcabd3d54
jamspawn <jamosquera8@misena.edu.co>
2015-01-22 19:30:06 -0500

    Jerarquia

--- client/modules/rawreferlist/rawreferlist.js
+++ client/modules/rawreferlist/rawreferlist.js
@@ -1,21 +1,110 @@
 Template.rawreferlist.helpers({
-	'multipliers' : function(){
+	'zone' : function(){
 		var iusr = Meteor.users.find({_id:Meteor.userId()}).fetch();
 		var tipo = iusr[0].profile.tipo;
 		var zone = iusr[0].profile.zona;
 		var comu = iusr[0].profile.comu;
 		var cuad = iusr[0].profile.cuad;
 		if(tipo == "Lider Cuadrante"){
-			var p = Prospectos.find({esMulti:true, cuadrante:cuad});
+			var p = Areas.find({tipo:'Zona',codigo:zone});
 		}
 		else if(tipo == "Lider Comuna"){
-			var p = Prospectos.find({esMulti:true, comuna:comu});
+			var p = Areas.find({tipo:'Zona',codigo:zone});
 		}
 		else if(tipo == "Lider Zona"){
-			var p = Prospectos.find({esMulti:true, zona:zone});
+			var p = Areas.find({tipo:'Zona',codigo:zone});
 		}
 		else if(tipo == "techsup" || tipo == "admin"){
-			var p = Prospectos.find({esMulti:true});
+			var p = Areas.find({tipo:'Zona'});
+		}
+		
+		return p;
+	},
+
+	'comune' : function(pzone){
+		var iusr = Meteor.users.find({_id:Meteor.userId()}).fetch();
+		var tipo = iusr[0].profile.tipo;
+		var zone = iusr[0].profile.zona;
+		var comu = iusr[0].profile.comu;
+		var cuad = iusr[0].profile.cuad;
+		if(tipo == "Lider Cuadrante"){
+			var p = Areas.find({tipo:'Comuna',codigo:comu});
+		}
+		else if(tipo == "Lider Comuna"){
+			var p = Areas.find({tipo:'Comuna',codigo:comu});
+		}
+		else if(tipo == "Lider Zona"){
+			var p = Areas.find({tipo:'Comuna',zona:zone});
+		}
+		else if(tipo == "techsup" || tipo == "admin"){
+			var p = Areas.find({tipo:'Comuna',zona:pzone});
+		}
+		
+		return p;
+	},
+
+	'quadrant' : function(comune){
+		var iusr = Meteor.users.find({_id:Meteor.userId()}).fetch();
+		var tipo = iusr[0].profile.tipo;
+		var zone = iusr[0].profile.zona;
+		var comu = iusr[0].profile.comu;
+		var cuad = iusr[0].profile.cuad;
+		if(tipo == "Lider Cuadrante"){
+			var p = Areas.find({tipo:'Cuadrante',codigo:cuad});
+		}
+		else if(tipo == "Lider Comuna"){
+			var p = Areas.find({tipo:'Cuadrante',comuna:comune});
+		}
+		else if(tipo == "Lider Zona"){
+			var p = Areas.find({tipo:'Cuadrante',comuna:comune});
+		}
+		else if(tipo == "techsup" || tipo == "admin"){
+			var p = Areas.find({tipo:'Cuadrante',comuna:comune});
+		}
+		
+		return p;
+	},
+
+	'cuadleaders' : function(pcuad){
+		var iusr = Meteor.users.find({_id:Meteor.userId()}).fetch();
+		var tipo = iusr[0].profile.tipo;
+		var zone = iusr[0].profile.zona;
+		var comu = iusr[0].profile.comu;
+		var cuad = iusr[0].profile.cuad;
+		if(tipo == "Lider Cuadrante"){
+			var p = Meteor.users.find({cuadrante:pcuad});
+		}
+		else if(tipo == "Lider Comuna"){
+			var p = Meteor.users.find({cuadrante:pcuad});
+		}
+		else if(tipo == "Lider Zona"){
+			var p = Meteor.users.find({cuadrante:pcuad});
+		}
+		else if(tipo == "techsup" || tipo == "admin"){
+			var p = Meteor.users.find({cuadrante:pcuad});
+		}
+		
+		return p;
+	},
+
+	'multipliers' : function(cl){
+		var iusr = Meteor.users.find({_id:Meteor.userId()}).fetch();
+		var tipo = iusr[0].profile.tipo;
+		var zone = iusr[0].profile.zona;
+		var comu = iusr[0].profile.comu;
+		var cuad = iusr[0].profile.cuad;
+		var cp = iusr[0].profile.cc;
+		if(tipo == "Lider Cuadrante"){
+			var p = Prospectos.find({esMulti:true, creadoPor:cp});
+		}
+		else if(tipo == "Lider Comuna"){
+			var p = Prospectos.find({esMulti:true, creadoPor:cl});
+		}
+		else if(tipo == "Lider Zona"){
+			var p = Prospectos.find({esMulti:true, creadoPor:cl});
+		}
+		else if(tipo == "techsup" || tipo == "admin"){
+			var p = Prospectos.find({esMulti:true, creadoPor:cl});
 		}
 		
 		return p;
@@ -48,6 +137,9 @@ Template.rawreferlist.helpers({
 
 Template.rawreferlist.rendered = function(){
 	listraw = Meteor.subscribe('prospectos');
+	listrawar = Meteor.subscribe('areas');
+	listrawcol = Meteor.subscribe('colaboradoresDatos');
+	//colaboradoresDatos
 }
 
 Template.rawreferlist.events({
